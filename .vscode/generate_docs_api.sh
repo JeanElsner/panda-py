7124dbbb170948e376bae186eab0fe180fe601cf
JeanElsner <jean.elsner@googlemail.com>
2023-08-04 18:51:46 +0200

    Fix pip package name in doc generation script

--- .vscode/generate_docs_api.sh
+++ .vscode/generate_docs_api.sh
@@ -1,4 +1,4 @@
-package_name="panda_py"
+package_name="panda-python"
 
 if metadata=$(pip show "$package_name" 2>/dev/null); then
     if [[ $metadata == *"Editable"* ]]; then
@@ -6,7 +6,7 @@ if metadata=$(pip show "$package_name" 2>/dev/null); then
         exit 1
     else
         location=$(pip show -f "$package_name" | awk '/^Location:/ {print $2}')
-        sphinx-apidoc -e -d 1 -M -T -f -o docs $location/panda_py
+        sphinx-apidoc -e -d 1 -M -T -f -o docs $location/panda_python
     fi
 else
     echo -e "\e[0;31mPackage '$package_name' is not installed. \e[0m"
