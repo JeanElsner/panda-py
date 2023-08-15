e62ea88b73a0c72ea8a4d82e39c8c9125ce32d23
JeanElsner <jean.elsner@googlemail.com>
2023-08-16 01:04:27 +0200

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'panda-py'
 copyright = '2023, Jean Elsner'
 author = 'Jean Elsner'
-release = '0.5.2'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
