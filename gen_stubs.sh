43ab8031adb81450e53e7d6b4158de0a7169452d
JeanElsner <jean.elsner@googlemail.com>
2024-07-04 16:59:47 +0200

    chore: move stubs script to bin

--- gen_stubs.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-pybind11-stubgen -o src --enum-class-locations RealtimeConfig:panda_py.libfranka --enum-class-locations VacuumGripperProductionSetupProfile:panda_py.libfranka --numpy-array-use-type-var panda_py
